8d527ccb17b2dcc14b5c6196941040783a51550c
Derek Maly <dmaly18@gmail.com>
2023-12-07 14:30:49 -0800

    another update

--- src/pages/DogShow.js
+++ src/pages/DogShow.js
@@ -1,21 +1,3 @@
-import React from 'react';
-import { useParams } from "react-router-dom"
 
-const DogShow = ({dogs}) => {
-  const {id} = useParams()
-  let selectedDog = dogs.find(dogObject => dogObject.id === +id)
 
-  return(
-    <div className="show-page"> 
-      {selectedDog && (
-        <>
-          <h2>I am {selectedDog.name}, age {selectedDog.age}.</h2>
-          <h3>I enjoy {selectedDog.enjoys}</h3>
-          <img alt={selectedDog.name} src={selectedDog.image}/>
-        </>
-      )}
-    </div>
-  )
-}
 
-export default DogShow
