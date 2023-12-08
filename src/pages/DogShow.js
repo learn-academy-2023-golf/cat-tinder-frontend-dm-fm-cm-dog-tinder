66de6a6217392258263def59f4c88ff4a1ad08ee
Derek Maly <dmaly18@gmail.com>
2023-12-08 11:26:40 -0800

    made updates

--- src/pages/DogShow.js
+++ src/pages/DogShow.js
@@ -1,9 +1,6 @@
 import { useParams } from "react-router-dom"
 
-const DogShow = ({dogs}) => {
-    const {id} = useParams()
-    let selectedDog = dogs.find(dogObject => dogObject.id === +id)
-}  
+
 
 
 export default DogShow
